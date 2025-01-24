9d323b666201be060ce2750944292fb83a8005e9
mookie <poolerg1@udayton.edu>
2025-01-24 10:17:50 -0500

    removed --ext from eslint scripts

---
+++ eslint.config.js
@@ -0,0 +1,10 @@
+// eslint.config.js
+export default [
+  {
+    files: ["**/*.ts"], // This targets all TypeScript files in your project
+    rules: {
+      semi: "error",
+      "prefer-const": "error"
+    }
+  }
+];
